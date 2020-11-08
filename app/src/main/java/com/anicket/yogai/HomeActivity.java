77eda6ca490116992824dd6cd73729c1ddbf48cd
Aniket Tatte <tatteaniket1506@gmail.com>
2020-11-08 21:35:18 +0530

    Created Layout For Profile screen.

--- app/src/main/java/com/anicket/yogai/HomeActivity.java
+++ app/src/main/java/com/anicket/yogai/HomeActivity.java
@@ -2,9 +2,11 @@ package com.anicket.yogai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
@@ -17,8 +19,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
         bottomNavigationBar= findViewById(R.id.bottomNavigationBar);
         bottomNavigationBar.setItemSelected(R.id.home,true);
+
+
+
         // Listener to handle changes on selecting new tile
         bottomNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
             @Override
@@ -26,21 +33,25 @@ public class HomeActivity extends AppCompatActivity {
                 switch(i) {
                     case R.id.home :
                         startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        overridePendingTransition(0,0);
                         finish();
                         break;
 
                     case R.id.meditation :
                         startActivity(new Intent(getApplicationContext(),MeditationActivity.class));
+                        overridePendingTransition(0,0);
                         finish();
                         break;
 
                     case R.id.yoga :
                         startActivity(new Intent(getApplicationContext(),YogaActivity.class));
+                        overridePendingTransition(0,0);
                         finish();
                         break;
 
                     case R.id.profile :
                         startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                        overridePendingTransition(0,0);
                         finish();
                         break;
                 }
